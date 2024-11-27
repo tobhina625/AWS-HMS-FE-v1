501abb267a1d816305dab5f716f1fdcc8fb5d4af
tobhina625 <faizanbutt.payoneer@gmail.com>
2024-11-27 23:05:50 +0500

    Sign in page fixed along with its validation

--- src/views/Authentication/SignupView.vue
+++ src/views/Authentication/SignupView.vue
@@ -2,11 +2,12 @@
 import DefaultAuthCard from '@/components/Auths/DefaultAuthCard.vue'
 import InputGroup from '@/components/Auths/InputGroup.vue'
 import BreadcrumbDefault from '@/components/Breadcrumbs/BreadcrumbDefault.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import FullScreenLayout from '@/layouts/FullScreenLayout.vue'
 
 import { ref } from 'vue'
 import GenderSelectionGroup from '../../components/Forms/GenderSelectionGroup.vue'
 import SpecializationSelectionGroup from '../../components/Forms/SpecializationSelectionGroup.vue'
+
 
 const selectedOption = ref<string>('')
 const isOptionSelected = ref<boolean>(false)
@@ -14,83 +15,106 @@ const isOptionSelected = ref<boolean>(false)
 const changeTextColor = () => {
   isOptionSelected.value = true
 }
+
 const pageTitle = ref('Sign Up')
+const signupPageData = ref({
+  fName: '',
+  lName: '',
+  email: '',
+  age: '',
+  cnic: '',
+  phoneNo: '',
+  gender: '',
+  address: '',
+  specialization: '',
+  password: '',
+  rePassword: '',
+})
+
+const signup = () => {
+  const isValid = validateRequiredFields();
+
+}
+
+const validateRequiredFields = () => {
+  if (
+    signupPageData.value.fName && 
+    signupPageData.value.lName &&
+    signupPageData.value.email
+  ) {
+
+  }
+}
 </script>
 
 <template>
-  <DefaultLayout>
-    <!-- Breadcrumb Start -->
-    <BreadcrumbDefault :pageTitle="pageTitle" />
-    <!-- Breadcrumb End -->
-
+  <FullScreenLayout>
     <DefaultAuthCard title="Sign Up" >
       <form>
         <div class="flex justify-between items-center">
           <p class="font-medium">
-            <InputGroup label="First Name" type="text" placeholder="First name" required>
+            <InputGroup label="First Name" type="text" placeholder="your first name" v-model="signupPageData.fName">
             </InputGroup>
           </p>
 
           <p class="font-medium">
-            <InputGroup label="Last Name" type="text" placeholder="Last name">
+            <InputGroup label="Last Name" type="text" placeholder="your fast name" v-model="signupPageData.lName">
             </InputGroup>
           </p>
         </div>
 
-        <InputGroup label="Email" type="email" placeholder="Email">
+        <InputGroup label="Email" type="email" placeholder="your email" v-model="signupPageData.email">
         </InputGroup>
         
         <div class="flex justify-between items-center">
           <p class="font-medium">
-            <InputGroup label="Age" type="number" placeholder="Enter your email">
+            <InputGroup label="Age" type="number" placeholder="your age" v-model="signupPageData.age">
             </InputGroup>
 
-            <InputGroup label="CNIC" type="email" placeholder="Enter your email">
+            <InputGroup label="CNIC" type="number" placeholder="your cnic" v-model="signupPageData.cnic">
             </InputGroup>
           </p>
 
           <p class="font-medium">
-            <InputGroup label="Phone No." type="tel" placeholder="Enter your phone number">
+            <InputGroup label="Phone No." type="tel" placeholder="your phone number" v-model="signupPageData.phoneNo">
             </InputGroup>
 
-            <GenderSelectionGroup />
+            <GenderSelectionGroup v-model="signupPageData.gender"/>
           </p>
         </div>
 
-        <InputGroup label="Address" type="email" placeholder="Enter your email">
+        <InputGroup label="Address" type="text" placeholder="your address" v-model="signupPageData.address">
         </InputGroup>
 
-        <SpecializationSelectionGroup />
+        <SpecializationSelectionGroup v-model="signupPageData.specialization"/>
 
         <div class="flex justify-between items-center">
           <p class="font-medium">
-            <InputGroup label="Password" type="password" placeholder="Enter your password">
+            <InputGroup label="Password" type="password" placeholder="Enter your password" v-model="signupPageData.password">
             </InputGroup>
           </p>
 
           <p class="font-medium">
-            <InputGroup label="Re-type Password" type="password" placeholder="Re-enter your password">
+            <InputGroup label="Re-type Password" type="password" placeholder="Re-enter your password" v-model="signupPageData.rePassword">
             </InputGroup>
           </p>
         </div>
 
         <div class="mb-5 mt-6">
-          <router-link to="/auth/signup">
-          <input
-            type="submit"
-            value="Sign Up"
-            class="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-opacity-90"
-          />
-          </router-link>
+          <div
+            class="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-opacity-90 text-center"
+            @click="signup()">
+            Sign Up
+          </div>
         </div>
 
         <div class="mt-6 text-center">
           <p class="font-medium">
             Already have an account?
-            <router-link to="/auth/signin" class="text-primary">Signin Here</router-link>
+            <router-link to="/auth/signin" class="text-primary">Sign In</router-link>
           </p>
         </div>
       </form>
     </DefaultAuthCard>
-  </DefaultLayout>
+  </FullScreenLayout>
 </template>
